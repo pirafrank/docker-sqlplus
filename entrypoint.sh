b6cfbbb3ac242fd789e9ed156932c8e8afd55003
Francesco Pira <dev@fpira.com>
2021-01-22 21:11:11 +0100

    go to interactive shell instead of sqlplus at start

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -e
+#set -e
 
 export ORACLE_HOME=/opt/instantclient
 export LD_LIBRARY_PATH=$ORACLE_HOME
 export SQLPATH=$ORACLE_HOME
 export PATH=$ORACLE_HOME:$PATH
 
-sleep 0.1s;
-rlwrap sqlplus $@
+#sleep 0.1s;
+#rlwrap sqlplus $@
+bash
+
